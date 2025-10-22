0340e2f3c6ed9d9a2b79bfb06d55da4d32d68d29
Juergen Wiemers <juergen.wiemers@gmail.com>
2025-10-22 12:07:40 +0200

    Revert to previous name

--- src/_gettsim_personas/de/grundsicherung_für_erwerbsfähige/__init__.py
+++ src/_gettsim_personas/de/grundsicherung_für_erwerbsfähige/__init__.py
@@ -14,7 +14,7 @@ Single0Children = OrigPersonaOverTime(
 )
 
 Single1Children = OrigPersonaOverTime(
-    path_to_persona_elements=Path(__file__).parent / "single_1_children.py",
+    path_to_persona_elements=Path(__file__).parent / "single_1_child.py",
     start_date=datetime.date(2005, 1, 1),
     error_if_not_implemented=(
         "These personas are available from 2005 because basic income support is not "
@@ -69,7 +69,7 @@ Couple0Children = OrigPersonaOverTime(
 )
 
 Couple1Children = OrigPersonaOverTime(
-    path_to_persona_elements=Path(__file__).parent / "couple_1_children.py",
+    path_to_persona_elements=Path(__file__).parent / "couple_1_child.py",
     start_date=datetime.date(2005, 1, 1),
     error_if_not_implemented=(
         "These personas are available from 2005 because basic income support is not "
